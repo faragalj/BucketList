1ae0987f37519a87818ac352e81e8223a8fb77a3
Joseph Farag Alla <faragalj@mcmaster.ca>
2020-09-01 22:15:21 -0400

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Joseph Faragalla on 2020-09-01.
+//  Copyright © 2020 Joseph Farag Alla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
