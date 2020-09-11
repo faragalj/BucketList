69de0e6c18dd36b426926e1330e57d91d4c63d27
Joseph Farag Alla <faragalj@mcmaster.ca>
2020-09-11 16:51:36 -0400

    finished tutorial I think?

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Joseph Faragalla on 2020-09-01.
 //  Copyright © 2020 Joseph Farag Alla. All rights reserved.
 
+import MapKit
 import SwiftUI
 import LocalAuthentication
 
@@ -181,6 +182,53 @@ struct UsingFaceIDTouchID: View {
             }
         } else {
             //no biometrics (ex ipod)
+        }
+    }
+}
+
+
+struct MoreOfMapKit: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    
+    var body: some View {
+        ZStack {
+            MapView2(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        //create a new location
+                        
+                        //when the button is tapped, add a marker where the center circle is
+                        let newLocation = MKPointAnnotation()
+                        newLocation.title = "Example Location"
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                .padding()
+                    .background(Color.black.opacity(0.7))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown Title"), message: Text(selectedPlace?.subtitle ?? "Missing Place information"), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                //edit here
+            })
         }
     }
 }
